c34aa92b2e76afb9bab765cdcd721fc819b6c273
karek314 <karek314@protonmail.ch>
2016-02-18 21:51:56 +0100

    Update bootloader-docker-build.sh

--- bootloader-docker-build.sh
+++ bootloader-docker-build.sh
@@ -4,16 +4,18 @@ FIRMWARETAG=${1:-master}
 
 docker build -t $IMAGETAG .
 docker run -t -v $(pwd)/output:/output $IMAGETAG /bin/sh -c "\
-	git clone https://github.com/trezor/trezor-mcu && \
-	cd trezor-mcu && \
-	git checkout $FIRMWARETAG && \
-	git submodule update --init && \
-	make -C vendor/libopencm3 && \
-	make && \
-	make -C bootloader && \
-	cp bootloader/bootloader.bin /output/bootloader-$FIRMWARETAG.bin"
+        git clone https://github.com/trezor/trezor-mcu && \
+        cd trezor-mcu && \
+        git checkout $FIRMWARETAG && \
+        git submodule update --init && \
+        make -C vendor/libopencm3 && \
+        make && \
+        make -C bootloader && \
+        cp bootloader/bootloader.bin /output/bootloader-$FIRMWARETAG.bin"
 
 echo "---------------------"
 echo "Bootloader fingerprint:"
-
-sha256sum output/bootloader-$FIRMWARETAG.bin
+FILENAME=output/bootloader-$FIRMWARETAG.bin
+sha256sum $FILENAME
+FILESIZE=$(stat -c%s "$FILENAME")     
+echo "Bootloader size: $FILESIZE/32000 bytes"
