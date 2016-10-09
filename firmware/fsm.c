e0587e1d20497a7f52f5e2bdc75d86a82017ced7
Daira Hopwood <daira@jacaranda.org>
2016-10-09 20:38:51 +0100

    Add support for multi-byte address prefixes. Assumes support in trezor-crypto (https://github.com/trezor/trezor-crypto/commit/8764e26368a639f9bd34fa1c8e4378dde938589d).
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- firmware/fsm.c
+++ firmware/fsm.c
@@ -757,11 +757,13 @@ void fsm_msgVerifyMessage(VerifyMessage *msg)
 	const CoinType *coin = fsm_getCoin(msg->coin_name);
 	if (!coin) return;
 	layoutProgressSwipe("Verifying", 0);
-	uint8_t addr_raw[21];
-	if (!ecdsa_address_decode(msg->address, addr_raw)) {
+	uint8_t addr_raw[MAX_ADDR_RAW_SIZE];
+	uint32_t address_type;
+	if (!getAddressType(coin, (const uint8_t *) msg->address, &address_type) || !ecdsa_address_decode(msg->address, address_type, addr_raw)) {
 		fsm_sendFailure(FailureType_Failure_InvalidSignature, "Invalid address");
+		return;
 	}
-	if (msg->signature.size == 65 && cryptoMessageVerify(coin, msg->message.bytes, msg->message.size, addr_raw, msg->signature.bytes) == 0) {
+	if (msg->signature.size == 65 && cryptoMessageVerify(coin, msg->message.bytes, msg->message.size, address_type, addr_raw, msg->signature.bytes) == 0) {
 		layoutVerifyAddress(msg->address);
 		if (!protectButton(ButtonRequestType_ButtonRequest_Other, false)) {
 			fsm_sendFailure(FailureType_Failure_ActionCancelled, "Message verification cancelled");
@@ -943,7 +945,7 @@ void fsm_msgEncryptMessage(EncryptMessage *msg)
 	RESP_INIT(EncryptedMessage);
 	const CoinType *coin = 0;
 	const HDNode *node = 0;
-	uint8_t address_raw[21];
+	uint8_t address_raw[21]; // FIXME: assumes addresses are 21 bytes
 	if (signing) {
 		coin = coinByName(msg->coin_name);
 		if (!coin) {
