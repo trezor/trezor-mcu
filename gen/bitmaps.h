047ffe52ca699ee2b67f8114e1f4f2f0bd731a9c
Peter Banik <peter@froggle.org>
2017-06-02 00:35:24 +0200

    Added Decred logo

--- gen/bitmaps.h
+++ gen/bitmaps.h
@@ -8,6 +8,7 @@ typedef struct {
 	const uint8_t *data;
 } BITMAP;
 
+extern const BITMAP bmp_decred;
 extern const BITMAP bmp_digit0;
 extern const BITMAP bmp_digit1;
 extern const BITMAP bmp_digit2;
